30306e0f1f98afd56cb4c3ee39670ee61216b8ac
thegrims <grimshawaidan@gmail.com>
2018-02-18 09:09:14 -0800

    teset

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,26 +1,33 @@
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
+import java.nio.*;
+import java.util.*;
+import java.lang.*;
 
 public class HelloWorld {
 	public static void main(String[] args) {
+		System.out.println(args[2]);
+		// throw new RunTimeException("alsdkn");
 		//System.out.println("Hello World Treehacks 2018!");
 		//System.out.println(args[0]);
-		try {
-	     File file = new File("newfile.txt");
-	     /*If file gets created then the createNewFile() 
-	      * method would return true or if the file is 
-	      * already present it would return false
-	      */
-             boolean fvar = file.createNewFile();
-	     if (fvar){
-	          System.out.println("File has been created successfully");
-	     }
-	     else{
-	          System.out.println("File already present at the specified location");
-	     }
-    	} catch (IOException e) {
-    		System.out.println("Exception Occurred:");
-	        e.printStackTrace();
-	    }
+		// try {
+	  //    File file = new File("newfile.txt");
+		// 	 List<String> lines = Arrays.asList(args[0]);
+		// 	 Path file2 = Paths.get("newfile.txt");
+		// 	 Files.write(file2, lines, Charset.forName("UTF-8"));
+	  //    /*If file gets created then t;he createNewFile()
+	  //     * method would return true or if the file is
+	  //     * already present it would return false
+	  //     */
+    //          boolean fvar = file.createNewFile();
+	  //    if (fvar){
+	  //         System.out.println("File has been created successfully");
+	  //    }
+	  //    else{
+	  //         System.out.println("File already present at the specified location");
+	  //    }
+    // 	} catch (IOException e) {
+    // 		System.out.println("Exception Occurred:");
+	  //       e.printStackTrace();
+	  //   }
 	}
 }
